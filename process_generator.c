8095f552697c1823077cdc71e1f17ef23b1389ee
AbdoWise-z <moh2002.abdow@gmail.com>
2023-04-13 11:30:26 +0200

    pq

--- process_generator.c
+++ process_generator.c
@@ -1,6 +1,10 @@
 #include "headers.h"
+#include <string.h>
+#include <ctype.h>
 
 void clearResources(int);
+
+
 
 int main(int argc, char * argv[])
 {
@@ -10,6 +14,20 @@ int main(int argc, char * argv[])
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
     // 3. Initiate and create the scheduler and clock processes.
     // 4. Use this function after creating the clock process to initialize clock
+
+    FILE* cfg = fopen("processes.txt" , "r");
+    int i , j = 0;
+    char buff[1024];
+    char num_buffer[128];
+
+    while (fgets(buff , 1024 , cfg) != NULL){
+        i = 0;
+        while (isspace(buff[i])) {i++;}
+        if (buff[i] == '#') continue;
+        //read data
+         
+    }
+
     initClk();
     // To get time use this
     int x = getClk();
@@ -20,6 +38,8 @@ int main(int argc, char * argv[])
     // 7. Clear clock resources
     destroyClk(true);
 }
+
+
 
 void clearResources(int signum)
 {
